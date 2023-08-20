98caba817ca1516d89e0d2b5fdcb5ea658de1619
00ROHIT00 <rohithr01092003@gmail.com>
2023-08-20 15:42:40 +0530

    Made Minor Changes to PHP File

--- php/add.php
+++ php/add.php
@@ -1,6 +1,7 @@
 <?php
 $movieName = $_POST['movieName'];
 $genre = $_POST['genre'];
+$lowerCase = strtolower($_POST['genre']); /*MAKES THE INPUT TO LOWERCASE*/
 $rating = $_POST['rating'];
 $releaseYear = $_POST['releaseYear'];
 
@@ -11,10 +12,10 @@ if($conn -> connect_error) {
   die("Connection Failed :".$conn->connect_error);
 } else {
   $stmt = $conn -> prepare("insert into movieData(movieName, genre, rating, releaseYear) values(?, ?, ?, ?)");
-  $stmt->bind_param("ssii",$movieName, $genre, $rating, $releaseYear);
+  $stmt->bind_param("ssii",$movieName, $lowerCase, $rating, $releaseYear);
   $execval = $stmt->execute();
   echo $execval;
-  echo "The Movie Was Added Successfully...";
+  echo '<script>alert("The Movie Was Added To The Database Successfully!")</script>';
   $stmt->close();
   $conn->close();
 }
