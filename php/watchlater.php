641cb59b8b257318be4b3ac180e35c2d6cc18b02
00ROHIT00 <rohithr01092003@gmail.com>
2023-10-05 19:41:16 +0530

    Made Some Changes To The Fonts In The Files - Stable State

--- php/watchlater.php
+++ php/watchlater.php
@@ -1,30 +1,29 @@
 <?php
-session_start();
-
 if (isset($_POST['movieName'])) {
-    $movieName = $_POST['movieName'];
-    echo "Received Movie Name:", $movieName , "<br>";
-    if (isset($_SESSION['userName'])) {
-        $username = $_SESSION['userName'];
-        $host = 'localhost';
-        $dbUsername = 'root';
-        $dbPassword = '';
-        $dbName = 'cinewise';
-        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $sqlAddToWatchLater = "INSERT INTO watchlater(username, movieName) VALUES('$username','$movieName');";
-        if ($conn->query($sqlAddToWatchLater) === TRUE) {
-            echo "Movie added to Watch Later successfully.";
-        } else {
-            echo "Error: " . $sqlAddToWatchLater . "<br>" . $conn->error;
-        }
-        $conn->close();
-    } else {
-        echo '<script>alert("User Is Not Signed In!");</script>';
+  $movieName = $_POST['movieName'];
+  echo "Received Movie Name:", $movieName;
+  if (isset($_SESSION['userName'])) {
+    $username = $_SESSION['userName'];
+    $host = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cinewise';
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
     }
+    $sqlAddToWatchLater = "INSERT INTO watchlater(username, movieName) VALUES('$username','$movieName');";
+    if ($conn->query($sqlAddToWatchLater) === TRUE) {
+      echo "Movie added to Watch Later successfully.";
+    } else {
+      echo "Error: " . $sqlAddToWatchLater . "<br>" . $conn->error;
+    }
+    $conn->close();
+  } else {
+    alert('User Is Not Signed In');
+    echo '<a href="login.php">Sign In</a>';
+  }
 } else {
-    echo "Movie Name not provided.";
+  echo "Movie Name not provided.";
 }
 ?>
